889b864df3c0eaf4023ab6a3c4229bfd8bae54aa
Garimathaku <garimathakur397@gmail.com>
2025-03-05 00:15:08 +0530

    [Garima]Add.validation with Regex

---
+++ EmployeePayroll.js
@@ -0,0 +1,34 @@
+class EmployeePayroll {
+    constructor(id, name, salary, gender, startDate) {
+        this.id = id;
+        this.salary = salary;
+        this.gender = gender;
+        this.startDate = new Date(startDate);
+        this.setName(name);
+    }
+
+    setName(name) {
+        try {
+            const nameRegex = /^[A-Z][a-zA-Z]{2,}$/;
+            if (!nameRegex.test(name)) {
+                throw new Error("Invalid Name! Name must start with a capital letter and have at least 3 characters.");
+            }
+            this.name = name;
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+   
+    getDetails = () => 
+        `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}, Gender: ${this.gender}, Start Date: ${this.startDate.toDateString()}`;
+}
+
+
+const emp1 = new EmployeePayroll(101, "Garima", 5000, "Female", "2023-05-10");  
+const emp2 = new EmployeePayroll(102, "Aaru", 7000, "Female", "2022-11-15"); 
+const emp3 = new EmployeePayroll(103, "Abhi", 6000, "Male", "2024-01-20"); 
+
+console.log(emp1.getDetails());
+console.log(emp2.getDetails()); 
+console.log(emp3.getDetails()); 
